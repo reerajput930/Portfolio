3702a9a8e0ff242f9be00444818651e033de7c9d
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2022-04-13 22:23:51 +0530

    code uploaded

---
+++ logic.js
@@ -0,0 +1,10 @@
+console.log("hello")
+import TypeIt from "typeit";
+// most valuable package in js for typing effect on text
+
+
+new TypeIt(".multipleStrings", {
+    strings: ["This is a great string.", "But here is a better one."],
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
